d10748202738d59e4af96d4685eac98df5d4d5bf
francdomain <cfrancmail@gmail.com>
2024-08-31 18:19:55 +0100

    edited docker compose file

--- start-php-server.sh
+++ start-php-server.sh
@@ -2,8 +2,8 @@
 
 php artisan migrate --force
 php artisan db:seed --force
+php artisan config:cache
 php artisan cache:clear
-php artisan config:clear
 php artisan route:clear
 
 php artisan serve --host=0.0.0.0 --port=8000
